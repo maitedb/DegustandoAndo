5f63689bf35ef84716c0eaa6984d25a0056d5407
maitedb <maitedemontebecker@gmail.com>
2023-04-12 16:12:30 -0300

    agregada bd

--- DegustandoAndo/core/models.py
+++ DegustandoAndo/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Articulos(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=140, default="")
+    ubicacion = models.CharField(max_length=100)
+    cuerpo = models.TextField(default="")
+    autor = models.CharField(max_length=100, default="")
+    fecha = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.titulo} - {self.subtitulo} - {self.ubicacion}"
