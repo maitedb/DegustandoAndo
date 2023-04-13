aaa1519214bf9bf2c44b71bd49a1acf86a635ac3
maitedb <maitedemontebecker@gmail.com>
2023-04-13 15:01:03 -0300

    agregados 4 templates

--- DegustandoAndo/core/urls.py
+++ DegustandoAndo/core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include 
-from core.views import inicio
+from core.views import inicio, agregar, editar, eliminar, mostrar
 
 urlpatterns = [
-    path('', inicio, name='index')
+    path('', inicio, name='index'),
+    path('agregar/', agregar, name='agregar'),
+    path('editar/', editar, name='editar'),
+    path('eliminar/', eliminar, name='eliminar'),
+    path('mostar/', mostrar, name='mostrar'),
 ]
--- DegustandoAndo/core/views.py
+++ DegustandoAndo/core/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'core/index.html')
+
+def agregar(request):
+    return render(request, 'core/agregar_articulo.html')
+
+def mostrar(request):
+    return render(request, 'core/mostrar_articulo.html')
+
+def editar(request):
+    return render(request, 'core/editar_articulo.html')
+
+def eliminar(request):
+    return render(request, 'core/eliminar_articulo.html')
+
