87abae56c0485bfb3605cdc34e871c386f773134
maitedb <maitedemontebecker@gmail.com>
2023-04-21 15:31:27 -0300

    ACA HAY ERROR

--- DegustandoAndo/DegustandoAndo/settings.py
+++ DegustandoAndo/DegustandoAndo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,5 +130,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "index"  
 LOGOUT_REDIRECT_URL = "index"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+AUTH_USER_MODEL = 'user.User'
